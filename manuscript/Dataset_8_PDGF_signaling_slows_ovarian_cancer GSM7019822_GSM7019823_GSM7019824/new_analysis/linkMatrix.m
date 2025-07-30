adcadb4810a791a59306c3157020c8e338866483
James Cai <jamescai@genomezoo.net>
2025-07-29 20:32:57 -0500

    Create linkMatrix.m

---
+++ manuscript/Dataset_8_PDGF_signaling_slows_ovarian_cancer GSM7019822_GSM7019823_GSM7019824/new_analysis/linkMatrix.m
@@ -0,0 +1,32 @@
+% Number of items in Bag 1 and Bag 2
+n1 = 3;
+n2 = 4;
+
+numLinks = n1 * n2;
+numComb = 2^numLinks;
+
+bag1_labels = {'A','B','C'};
+bag2_labels = {'1','2','3','4'};
+
+for idx = 0:numComb-1
+    % Get binary vector for current combination
+    bits = bitget(idx, 1:numLinks);
+    % Reshape to 3x4 "link matrix"
+    linkmat = reshape(bits, n2, n1)';
+    
+    % Print the links for this combination
+    fprintf('Combination %d:\n', idx+1);
+    [r,c] = find(linkmat);
+    if isempty(r)
+        disp('  No links.');
+    else
+        for k = 1:numel(r)
+            fprintf('  Link: %s - %s\n', bag1_labels{r(k)}, bag2_labels{c(k)});
+        end
+    end
+    fprintf('\n');
+    
+    % Uncomment below to display the matrix as well
+    % disp(linkmat);
+    % pause
+end
