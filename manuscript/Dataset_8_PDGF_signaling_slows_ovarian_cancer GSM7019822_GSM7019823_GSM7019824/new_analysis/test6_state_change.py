94da8e810bb60ece7c2e1d1bd43be8a99c02fc84
James Cai <jamescai@genomezoo.net>
2025-08-03 15:16:53 -0500

    Update test6_state_change.py

--- manuscript/Dataset_8_PDGF_signaling_slows_ovarian_cancer GSM7019822_GSM7019823_GSM7019824/new_analysis/test6_state_change.py
+++ manuscript/Dataset_8_PDGF_signaling_slows_ovarian_cancer GSM7019822_GSM7019823_GSM7019824/new_analysis/test6_state_change.py
@@ -11,11 +11,11 @@ n = int(np.log2(len(psi_1)))
 qc = QuantumCircuit(n)
 
 # Un-preparation: psi_1^dagger (adjoint of the gate that prepares psi_1 from |0>)
-init1 = Initialize(psi_1)
+init1 = Initialize(np.sqrt(psi_1))
 init1_circ = init1.gates_to_uncompute()   # The inverse circuit
 
 # Preparation: prepare psi_2 from |0>
-init2 = Initialize(psi_2)
+init2 = Initialize(np.sqrt(psi_2))
 
 # Compose: first uncompute psi_1, then compute psi_2
 qc.append(init1_circ, range(n))
