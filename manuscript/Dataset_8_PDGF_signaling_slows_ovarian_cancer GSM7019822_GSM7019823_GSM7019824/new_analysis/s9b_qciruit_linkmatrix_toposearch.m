5cdd0e5a4bc8e31f34beaa48ea8eb6b1f844b57b
jamesjcai <jamescai@genomezoo.net>
2025-08-02 20:09:26 -0500

    Add state conversion notes and clean up toposearch script
    Added a detailed LaTeX note on arbitrary quantum state conversion and Qiskit implementation. Deleted the .asv autosave file. Refactored s9b_qciruit_linkmatrix_toposearch.m for clarity, removed commented/unused code, improved link annotation, and fixed minor formatting issues.

--- manuscript/Dataset_8_PDGF_signaling_slows_ovarian_cancer GSM7019822_GSM7019823_GSM7019824/new_analysis/s9b_qciruit_linkmatrix_toposearch.m
+++ manuscript/Dataset_8_PDGF_signaling_slows_ovarian_cancer GSM7019822_GSM7019823_GSM7019824/new_analysis/s9b_qciruit_linkmatrix_toposearch.m
@@ -3,32 +3,28 @@ addpath ../
 FibroblastGenes = ["TGFB1", "PDGFRB", "IL6"];
 CancerGenes = ["STAT3","IL6RorST","TGFBR1or2","PDGFB"];
 
-batquery = "Fibroblasts (Mo)";
-genquery = FibroblastGenes;
+batquery = "Fibroblasts (Mo)"; genquery = FibroblastGenes;
 [pt_f_mo, ~, ~, f0_f_mo] = getn(batquery, genquery, sce, false);
 
-batquery = "Cancer Cells (Mo)";
-genquery = CancerGenes;
+batquery = "Cancer Cells (Mo)"; genquery = CancerGenes;
 [pt_c_mo, ~, ~, f0_c_mo] = getn(batquery, genquery, sce, false);
 
-batquery = "Fibroblasts (Co)";
-genquery = FibroblastGenes;
+batquery = "Fibroblasts (Co)"; genquery = FibroblastGenes;
 [pt_f_co, ~, ~, ~] = getn(batquery, genquery, sce, false);
 
-batquery = "Cancer Cells (Co)";
-genquery = CancerGenes;
+batquery = "Cancer Cells (Co)"; genquery = CancerGenes;
 [pt_c_co, ~, ~, ~] = getn(batquery, genquery, sce, false);
-
 
 %% 
 % quantum state preparation via uniformly controlled rotations
 amp = sqrt(pt_f_mo);                % unnormalized amplitudes vector
+% log2(numel(pt_f_mo)) = 3
 cg_f = initGate(1:3, amp);          % creates CompositeGate to initialize qubits 1–3 :contentReference[oaicite:1]{index=1}
 qc_f = quantumCircuit(cg_f);        % initialize circuit with the gate
 
-amp = sqrt(pt_c_mo);               % unnormalized amplitudes vector
-cg_c = initGate(1:4, amp);         % creates CompositeGate to initialize qubits 1–3 :contentReference[oaicite:1]{index=1}
-qc_c = quantumCircuit(cg_c);         % initialize circuit with the gate
+amp = sqrt(pt_c_mo);                % unnormalized amplitudes vector
+cg_c = initGate(1:4, amp);          % creates CompositeGate to initialize qubits 1–4 :contentReference[oaicite:1]{index=1}
+qc_c = quantumCircuit(cg_c);        % initialize circuit with the gate
 
 
 %%
@@ -67,14 +63,12 @@ for idx = 1:numComb
     [~, po_c] = querystates(S,[4 5 6 7]);   % observed state pattern in cancer   
     kl1 = i_kldiverg(pt_f_co, po_f);
     kl2 = i_kldiverg(pt_c_co, po_c);
-    Y(idx) = kl1 + kl2;        
-    % Print the links for this combination
+    Y(idx) = kl1 + kl2;
     fprintf('Combination %d: %f\n', idx, Y(idx));
-    % fprintf('\n');
 end
 
 %%
-[a,b] = mink(Y,10)
+[a, b] = mink(Y,10)
 figure;
 plot(Y);
 
@@ -82,68 +76,28 @@ FibroblastGenes = ["TGFB1", "PDGFRB", "IL6"];
 CancerGenes = ["STAT3","IL6RorST","TGFBR1or2","PDGFB"];
 FCGenes = [FibroblastGenes CancerGenes];
 
+
 for topk = 1:3
 
 
     C = Cc{b(topk)};
     S = simulate(C);
-    % [~, po_f] = querystates(S,[1 2 3]);     % observed state pattern in fibroblast
-    % [~, po_c] = querystates(S,[4 5 6 7]);   % observed state pattern in cancer   
-    % kl1 = i_kldiverg(pt_f_co, po_f);
-    % kl2 = i_kldiverg(pt_c_co, po_c);
-    % Y(idx+1) = kl1 + kl2;        
-    
-    
-    
-    
-    
-    % layer_inte = [cryGate(5,4,0); ...
-    %         cryGate(1,6,0); ...
-    %         cryGate(7,2,0); ...
-    %         cryGate(3,5,0)];
-    
-    
-    
-    
-    
-    %{
-    n = size(layer_inte, 1);
-    inivalue = -pi + (2*pi)*rand(n, 1);
-    
-    methodid = 3;
-    switch methodid
-        case 1
-            options = optimset('Display','iter');
-            [optimtheta, fval] = fminunc(@i_obj, inivalue, options, pt_f_co, pt_c_co, C);       
-        case 2
-            options = optimset('Display','iter');
-            [optimtheta, fval] = fminsearch(@i_obj, inivalue, options, pt_f_co, pt_c_co, C);
-        case 3
-            options = optimoptions('fmincon','Display','iter');
-            lb = -pi*ones(n, 1); ub = pi*ones(n, 1);
-            [optimtheta, fval] = fmincon(@i_obj, inivalue, [], [], [], [], ...
-                lb, ub, [], options, pt_f_co, pt_c_co, C);
-    end
-    
-    
-    S = simulate(C);    
-    [states_f, po_f_mo] = querystates(S,[1 2 3]);     % observed state pattern in fibroblast
-    [states_c, po_c_mo] = querystates(S,[4 5 6 7]);   % observed state pattern in cancer   
-    
-    for k = 1:n
-        C.Gates(end-(k-1)).Angles = optimtheta(k);
-    end
-    %}
-    
-    S = simulate(C);
     [states_f2, po_f2] = querystates(S,[1 2 3]);     % observed state pattern in fibroblast
     [states_c2, po_c2] = querystates(S,[4 5 6 7]);   % observed state pattern in cancer   
-    
     
     figure;
     nexttile
     plot(C)
     
+    nexttile
+    for k = 1:length(configsK{b(topk)})
+        x = configsK{b(topk)}(k, 1) +" "+ FCGenes(configsK{b(topk)}(k, 1))+...
+            " -> "+configsK{b(topk)}(k, 2)+" "+FCGenes(configsK{b(topk)}(k, 2));
+        text(0, 50 - k*10, x);
+        ylim([0 50]);
+        axis off
+    end
+
     nexttile
     bar([pt_c_mo pt_c_co po_c2])
     set(gca,'XTick',1:length(states_c));
@@ -162,18 +116,6 @@ for topk = 1:3
     title('Fibroblasts')
     %legend({'Mono','Co','Simulated2'})
 
-    nexttile
-    for k = 1:length(configsK{topk})
-        if configsK{topk}(k, 1)<=3
-            a = "->";
-        else
-            a = "<-";
-        end
-        a = configsK{topk}(k, 1) + FCGenes(configsK{topk}(k, 1))+a+configsK{topk}(k, 2)+FCGenes(configsK{topk}(k, 2));
-        text(0, k*10, a);
-        ylim([0 50]);
-        axis off
 
-    end    
 
 end
