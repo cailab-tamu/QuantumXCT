510bd1491546c12f1a1f927476a9de495d8b168a
jamesjcai <jamescai@genomezoo.net>
2025-08-04 23:25:31 -0500

    .

---
+++ MATLAB/s9f_k3_linkmatrix_fwdbwd.m
@@ -0,0 +1,150 @@
+% forwad and backward KL div 
+
+a = pwd;
+targettop = [7 2; 1 6; 3 5];
+cd('../manuscript/Dataset_8_PDGF_signaling_slows_ovarian_cancer GSM7019822_GSM7019823_GSM7019824/new_analysis/');
+run('s0_merge_subunit_genes.m')
+addpath ./
+addpath ../
+cd(a);
+%%
+FibroblastGenes = ["TGFB1", "PDGFRB", "IL6"];
+CancerGenes = ["STAT3","IL6RorST","TGFBR1or2","PDGFB"];
+FCGenes = [FibroblastGenes CancerGenes];
+
+
+batquery = "Fibroblasts (Mo)"; genquery = FibroblastGenes;
+[pt_f_mo, ~, ~, ~] = getn(batquery, genquery, sce, false);
+
+batquery = "Cancer Cells (Mo)"; genquery = CancerGenes;
+[pt_c_mo, ~, ~, ~] = getn(batquery, genquery, sce, false);
+
+batquery = "Fibroblasts (Co)"; genquery = FibroblastGenes;
+[pt_f_co, ~, ~, ~] = getn(batquery, genquery, sce, false);
+
+batquery = "Cancer Cells (Co)"; genquery = CancerGenes;
+[pt_c_co, ~, ~, ~] = getn(batquery, genquery, sce, false);
+
+%% 
+% log2(numel(pt_f_mo)) = 3
+cg_f1 = initGate(1:3, sqrt(pt_f_mo));          % creates CompositeGate to initialize qubits 1–3 :contentReference[oaicite:1]{index=1}
+qc_f1 = quantumCircuit(cg_f1);        % initialize circuit with the gate
+
+cg_c2 = initGate(1:4, sqrt(pt_c_mo));          % creates CompositeGate to initialize qubits 1–4 :contentReference[oaicite:1]{index=1}
+qc_c2 = quantumCircuit(cg_c2);        % initialize circuit with the gate
+
+cg_f3 = initGate(1:3, sqrt(pt_f_co));          % creates CompositeGate to initialize qubits 1–3 :contentReference[oaicite:1]{index=1}
+qc_f3 = quantumCircuit(cg_f3);        % initialize circuit with the gate
+
+cg_c4 = initGate(1:4, sqrt(pt_c_co));          % creates CompositeGate to initialize qubits 1–4 :contentReference[oaicite:1]{index=1}
+qc_c4 = quantumCircuit(cg_c4);        % initialize circuit with the gate
+
+%%
+cg1_mapped = compositeGate(cg_f1, [1 2 3]);
+cg2_mapped = compositeGate(cg_c2, [4 5 6 7]);
+
+cg3_mapped = compositeGate(cg_f3, [1 2 3]);
+cg4_mapped = compositeGate(cg_c4, [4 5 6 7]);
+
+combinedGate = [cg1_mapped; cg2_mapped];
+C = quantumCircuit(combinedGate);
+S = simulate(C);    
+[states_f, po_f_mo] = querystates(S,[1 2 3]);     % observed state pattern in fibroblast
+[states_c, po_c_mo] = querystates(S,[4 5 6 7]);   % observed state pattern in cancer   
+
+
+% layer_entg = [cxGate(1, 2); cxGate(2, 3); cxGate(3, 1);...
+%     cxGate(4, 5); cxGate(5, 6); cxGate(6, 7);...
+%     cxGate(7, 4)];
+% [layer_base] = in_12layers([f0_f_mo; f0_c_mo]);
+
+%%
+configsK = linkMatrix_dir_k(3);
+numComb = length(configsK);
+
+Y = nan(numComb, 1);
+Cc = cell(numComb, 2);
+idealY = [];
+isideal = false(numComb, 1);
+
+for idx = 1:numComb
+
+    if any(ismember(fliplr(configsK{idx}), configsK{idx}, "rows")), continue; end
+
+    layer_inte = [];
+    for k = 1:height(configsK{idx})
+        layer_inte = [layer_inte; cxGate(configsK{idx}(k,1), configsK{idx}(k,2))];
+    end
+    combinedGate = [cg1_mapped; cg2_mapped; cxGate(4,5); layer_inte];
+    C = quantumCircuit(combinedGate);
+    Cc{idx, 1} = C;
+    S = simulate(C);
+    [~, po_f] = querystates(S,[1 2 3]);     % observed state pattern in fibroblast
+    [~, po_c] = querystates(S,[4 5 6 7]);   % observed state pattern in cancer   
+    kl1 = i_kldiverg(pt_f_co, po_f);
+    kl2 = i_kldiverg(pt_c_co, po_c);
+
+    layer_inte = [];
+    for k = 1:height(configsK{idx})
+        layer_inte = [layer_inte; cxGate(configsK{idx}(k,2), configsK{idx}(k,1))];
+    end
+    combinedGate = [cg3_mapped; cg4_mapped; cxGate(5,4); layer_inte];
+    C = quantumCircuit(combinedGate);
+    Cc{idx, 2} = C;
+    S = simulate(C);
+    [~, po_f] = querystates(S,[1 2 3]);     % observed state pattern in fibroblast
+    [~, po_c] = querystates(S,[4 5 6 7]);   % observed state pattern in cancer   
+    kl3 = i_kldiverg(pt_f_mo, po_f);
+    kl4 = i_kldiverg(pt_c_mo, po_c);
+
+    Y(idx) = kl1 + kl2 + kl3 + kl4;
+    fprintf('Combination %d: %f\n', idx, Y(idx));
+    if all(ismember(sort(configsK{idx},2), sort(targettop, 2),"rows"))
+        idealY = [idealY; Y(idx)];
+        isideal(idx) = true;
+    end
+
+end
+
+%%
+[a, b] = mink(Y, 10);
+figure;
+plot(Y);
+yline(idealY,'r');
+
+
+
+%%
+for topk = 1:3
+    t = fun_drawreshisto(b(topk), Cc(:,1), configsK, ...
+        pt_c_mo, pt_c_co, pt_f_mo, pt_f_co, ...
+        FCGenes, states_c, states_f, targettop);
+    title(t, sprintf('Numerically Best Configuration %d, KL = %f', ...
+        topk, Y(b(topk))));
+end
+
+%%
+
+% kx=0;
+% for topk = 1:numComb
+%     if ~isideal(topk), continue; end
+%     kx=kx+1;
+%     t = fun_drawreshisto(topk, Cc, configsK, ...
+%         pt_c_mo, pt_c_co, pt_f_mo, pt_f_co, ...
+%         FCGenes, states_c, states_f, targettop);
+%     title(t, sprintf('Biologically Ideal Configuration, KL = %f', idealY(kx)));
+% end
+
+[idealY_sorted, idx] = sort(idealY);
+a = find(isideal);
+a = a(idx);
+for topk = 1:length(a)
+    t = fun_drawreshisto(a(topk), Cc(:,1), configsK, ...
+        pt_c_mo, pt_c_co, pt_f_mo, pt_f_co, ...
+        FCGenes, states_c, states_f, targettop);
+    title(t, sprintf('Biologically Ideal Configuration %d, KL = %f', ...
+        topk, idealY_sorted(topk)));
+end
+
+
+
